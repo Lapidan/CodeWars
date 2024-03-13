5d307adbc34435fa92185785bb3ff830ea44ba0e
DmitryLapitski <lapidan2020@mail.ru>
2024-03-13 20:17:14 +0300

    easy-logs(js)

---
+++ 8kyu/easy-logs.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: 
+// log
+// ⁡
+// �
+// �
+// +
+// log
+// ⁡
+// �
+// �
+// log 
+// X
+// ​
+//  A+log 
+// X
+// ​
+//  B.
+
+//SOLUTION:
+
